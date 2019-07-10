a4ccff0d09d37144c5d093f7fd6b8342cdc71dc3
Adam Buxton <buxton.adam@gmail.com>
2019-07-10 16:26:06 +0100

    update service management

--- manifests/service.pp
+++ manifests/service.pp
@@ -4,17 +4,23 @@
 #
 # @example
 #   include sophos::service
-class sophos::service {
+class sophos::service (
+  Boolean                   $enable_service         = true,
+  Enum['stopped','running'] $ensure_agent           = 'running',
+  Enum['stopped','running'] $ensure_servicemanager  = 'running',
+  Enum['stopped','running'] $ensure_uiserver        = 'running',
+){
+
   service { 'com.sophos.agent':
-    ensure => 'stopped',
-    enable => 'true',
+    ensure => $ensure_agent,
+    enable => $enable_service,
   }
   service { 'com.sophos.common.servicemanager':
-    ensure => 'running',
-    enable => 'true',
+    ensure => $ensure_servicemanager,
+    enable => $enable_service,
   }
   service { 'com.sophos.endpoint.uiserver':
-    ensure => 'stopped',
-    enable => 'true',
+    ensure => $ensure_uiserver,
+    enable => $enable_service,
   }
 }
